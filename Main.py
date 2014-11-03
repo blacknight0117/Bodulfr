7597deac25edcf920111c3cf796c4557ff554fb1
Nathan Black <blacKnight0117@gmail.com>
2014-11-03 16:28:46 -0500

    Initialize

---
+++ Main.py
@@ -0,0 +1,16 @@
+__author__ = 'black_000'
+#TODO Splash screen - SKIP FOR NOW
+#TODO Main Menu -
+#TODO Options
+#TODO SinglePlayer Launch
+
+#needed stuff
+#Menu class
+#Popup class
+#Toolbar class
+#general layout with default functions
+
+#INGAME
+#environment class
+#enemy class
+#player class
